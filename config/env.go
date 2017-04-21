b5de907541b0ad1796677dd68442fd5244a3512d
Matthew Deloughry <matt@deloughry.co.uk>
2017-04-21 15:46:21 +0100

    further work

--- config/env.go
+++ config/env.go
@@ -1,14 +1,24 @@
 package config
 
 import (
+	"fmt"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func Env(item string) string {
-	err := godotenv.Load()
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
+		log.Fatal(err)
+	}
+
+	env_file := fmt.Sprintf("%s/.env", dir)
+
+	err2 := godotenv.Load(env_file)
+
+	if err2 != nil {
 		log.Fatal("Error loading .env file")
 	}
 
